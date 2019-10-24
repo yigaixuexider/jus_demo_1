8b22135fd720705b34c04b913de8765e038731a6
wm <15871491953@163.com>
2019-10-24 14:24:17 +0800

    2019年10月24日提交（我的问题目录得到解决）

--- src/main/java/com/play/majiang/jus_demo/controller/IndexController.java
+++ src/main/java/com/play/majiang/jus_demo/controller/IndexController.java
@@ -1,21 +1,12 @@
 package com.play.majiang.jus_demo.controller;
 
 import com.play.majiang.jus_demo.dto.PaginationDTO;
-import com.play.majiang.jus_demo.dto.QuestionDTO;
-import com.play.majiang.jus_demo.mapper.QuestionMapper;
-import com.play.majiang.jus_demo.mapper.UserMapper;
-import com.play.majiang.jus_demo.model.Question;
-import com.play.majiang.jus_demo.model.User;
 import com.play.majiang.jus_demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * created by wm on 2019/10/9
@@ -24,29 +15,14 @@ import java.util.List;
 @Controller
 
 public class IndexController {
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public  String index(HttpServletRequest request,
-                         Model model,
+    public  String index(Model model,
                          @RequestParam(name = "page",defaultValue = "1") Integer  page,
                          @RequestParam(name = "size",defaultValue = "5") Integer  size){
-        Cookie[] cookies = request.getCookies();
-        if(cookies !=null&& cookies.length!=0)
-        for(Cookie cookie : cookies){
-            if(cookie.getName().equals("token")){
-                String token=cookie.getValue();
-                User user=userMapper.findByToken(token);
-                if(user!=null){
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
         PaginationDTO pagination=questionService.list(page,size);
         model.addAttribute("pagination",pagination);
         return "index";
--- src/main/java/com/play/majiang/jus_demo/controller/PublicController.java
+++ src/main/java/com/play/majiang/jus_demo/controller/PublicController.java
@@ -1,7 +1,6 @@
 package com.play.majiang.jus_demo.controller;
 
 import com.play.majiang.jus_demo.mapper.QuestionMapper;
-import com.play.majiang.jus_demo.mapper.UserMapper;
 import com.play.majiang.jus_demo.model.Question;
 import com.play.majiang.jus_demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -23,8 +21,7 @@ public class PublicController {
     @Autowired
     private QuestionMapper questionMapper;
 
-    @Autowired
-    private UserMapper  userMapper;
+
 
      @GetMapping("/publish")
      public  String publish(){
@@ -53,19 +50,7 @@ public class PublicController {
              return "publish";
          }
 
-         User user=null;
-         Cookie[] cookies = request.getCookies();
-         if(cookies !=null&& cookies.length!=0)
-         for(Cookie cookie : cookies) {
-             if (cookie.getName().equals("token")) {
-                 String token = cookie.getValue();
-                 user = userMapper.findByToken(token);
-                 if (user != null) {
-                     request.getSession().setAttribute("user", user);
-                 }
-                 break;
-             }
-         }
+         User user=(User) request.getSession().getAttribute("user");
          if (user==null){
              model.addAttribute("error","用户未登录");
              return "publish";
